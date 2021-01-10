0b10cd34f05b4408a6e74ce250deaeef183aa23d
KyawMyo <mr.kyawmyooo@gmail.com>
2021-01-10 20:04:53 +0800

    initial code version 1.00

---
+++ idmtl.py
@@ -0,0 +1,39 @@
+# idmtl relay time calculator
+# equation for trip time
+
+# t = TMS(k / (PSM**a - 1))
+
+curve_type = {
+    "standard_inverse": {"k": 0.140, "a": 0.020},
+    "very_inverse": {"k": 13.5, "a": 1},
+    "extremely_inverse": {"k": 80, "a": 2},
+    "long_time_inverse": {"k": 120, "a": 1},
+}
+
+
+# print(curve_type["standard_inverse"]["a"])
+
+def trip_time(time_multiplier, a_const, k_const, plug_setting_multiplier):
+    time = time_multiplier*(k_const / ((plug_setting_multiplier ** a_const) - 1))
+    return round(time,3)
+
+
+user_chose_curve = int(input("Choose the curve type "
+                             "\ntype 1 for Standard Inverse"
+                             "\ntype 2 for Very Inverse"
+                             "\ntype 3 for Extremely Inverse"
+                             "\ntype 4 for Long Time Inverse\n"))
+user_chose_curve = user_chose_curve-1
+
+curve_list = ["standard_inverse", "very_inverse", "extremely_inverse", "long_time_inverse"]
+
+tms = float(input("what is TMS: "))
+relay_pickup_current = int(input("What is the relay pick up current: "))
+actual_fault_current = int(input("What is the fault current: "))
+
+psm = actual_fault_current / relay_pickup_current
+
+a = curve_type[curve_list[user_chose_curve]]["a"]
+k = curve_type[curve_list[user_chose_curve]]["k"]
+
+print(f"The Standard Tripping Time is {trip_time(tms, a, k, psm)} seconds")
